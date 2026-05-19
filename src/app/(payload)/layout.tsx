7b7de2d03ad4754a6b652f4361ff5b0763b7719d
Johannes Homann <hi@johanneshomann.de>
2026-05-19 16:09:40 +0200

    fix: use Payload RootLayout in (payload) layout to set up ConfigProvider

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -1,5 +1,15 @@
+import config from '@payload-config'
+import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
+import type { ServerFunctionClient } from 'payload'
 import React from 'react'
+import { importMap } from './admin/importMap'
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return children
+const serverFunction: ServerFunctionClient = async (args) => {
+  'use server'
+  return handleServerFunctions({ ...args, config, importMap })
 }
+
+const Layout = ({ children }: { children: React.ReactNode }) =>
+  RootLayout({ children, config, importMap, serverFunction })
+
+export default Layout
