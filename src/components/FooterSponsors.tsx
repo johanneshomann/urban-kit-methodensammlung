b3386d7ec716a19a08e725ff7d249a4604196e9a
Johannes Homann <hi@johanneshomann.de>
2026-08-14 15:01:11 +0800

    FIX - footer sponsor band: standard size to 110px

--- src/components/FooterSponsors.tsx
+++ src/components/FooterSponsors.tsx
@@ -23,7 +23,7 @@ function SponsorLogo({ sponsor }: { sponsor: Sponsor }) {
     <img
       src={sponsor.logoUrl}
       alt={sponsor.alt}
-      className={`w-auto object-contain ${sponsor.size === 'gross' ? 'h-40 max-w-[48rem]' : 'h-[6.25rem] max-w-[30rem]'}`}
+      className={`w-auto object-contain ${sponsor.size === 'gross' ? 'h-40 max-w-[48rem]' : 'h-[6.875rem] max-w-[32rem]'}`}
       loading="lazy"
     />
   )
